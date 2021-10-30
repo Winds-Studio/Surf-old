fa27b5f164b669b633385d7854f2781ef82dac48
Dreeam <61569423+Dreeam-qwq@users.noreply.github.com>
2021-10-31 01:47:10 +0800

    Update Bedrock.java

--- src/main/java/org/l2x9/l2x9core/listeners/patches/Bedrock.java
+++ src/main/java/org/l2x9/l2x9core/listeners/patches/Bedrock.java
@@ -15,6 +15,10 @@ import org.bukkit.event.world.ChunkLoadEvent;
 public class Bedrock implements Listener {
     Main plugin;
 
+    public Bedrock(Main plugin) {
+        this.plugin = plugin;
+    }
+
     @EventHandler
     private void onChunkLoadEvent(ChunkLoadEvent evt) {
         FileConfiguration config = plugin.getConfig();
